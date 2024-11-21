cc135a689f3d885291813a2787af67e7b4b34df5
nagobaa <fej@hotmail.fi>
2024-11-21 10:37:06 +0200

    Uppgift nummer 7

---
+++ övning 7.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace test
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+            // version 1 while-loop
+            Console.WriteLine("This is version 1 of the while-loop");
+
+			int counter = 1;
+
+			while (counter <= 5)
+			{
+				Console.WriteLine("The value of the int variable counter is:" + " " + counter);
+				counter += 1;
+			}
+
+			Console.WriteLine("The loop of version 1 has finished");
+			
+			// version 2 while-loop
+			Console.WriteLine("\nThis is version 2 of the while-loop");
+
+			int counter2 = 1;
+			
+			do
+			{
+				Console.WriteLine("The value of the int variable counter is:" + " " + counter2);
+				counter2 += 1;
+			} while (counter2 <= 5);
+            
+			Console.WriteLine("The loop of version 2 has finished");
+			Console.ReadKey();
+		}
+	}
+}
