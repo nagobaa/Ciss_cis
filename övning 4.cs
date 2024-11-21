0b4dd49b952a46739652cf5687f3470930d6141c
nagobaa <fej@hotmail.fi>
2024-11-21 09:21:44 +0200

    nya uppgifter

---
+++ övning 4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace text
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("What is your age? ");
+            string ageInput = Console.ReadLine();
+            int age = Convert.ToInt32(ageInput);
+
+            if (age < 18)
+            {
+                Console.WriteLine("Not eligibile to vote");
+            }
+            else
+            {
+                Console.WriteLine("Eligible to vote");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ övning 5.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //raden nedan kan inte vara innanför switch-slingan
+            Console.Write("Please enter a grade: ");
+            string gradeInput = Console.ReadLine();
+            int grade = Convert.ToInt32(gradeInput);
+
+            switch (grade)
+            {
+
+                case 5:
+                    Console.WriteLine("Excellent");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Good");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Satisfactory");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Ok");
+                    break;
+
+                case 1:
+                    Console.WriteLine("Needs Improvement");
+                    break;
+
+                case 0:
+                    Console.WriteLine("Fail");
+                    break;
+
+                default:
+                    Console.WriteLine("Please input a grade between 0-5: ");
+                    break; // hit ska också "break;"
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
+
