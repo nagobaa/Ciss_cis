df47a72893cb968c36f16b966c5a739128e5ab43
nagobaa <fej@hotmail.fi>
2024-11-21 09:46:46 +0200

    övning nummer 6

---
+++ övning 6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace test
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many times should the loop run? ");
+            string loopRunInput = Console.ReadLine();
+            int loopRun = Convert.ToInt32(loopRunInput);
+
+            for (int i = 1; i < (loopRun + 1); i++)
+
+            {
+                Console.WriteLine("Loopcounter" + " " + i);
+            }
+
+            Console.WriteLine("The loop has finished");
+            Console.ReadKey(); //detta ska in hit
+        }
+        // Console.ReadKey() // inte hit
+    }
+}
