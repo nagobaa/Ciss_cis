f7047969340a653204ac56de10ffb9a4cfdb73dd
nagobaa <fej@hotmail.fi>
2024-11-26 10:41:00 +0200

    Create övning 9.cs

---
+++ övning 9.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace test
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 1;
+            int num2 = 2;
+            int num3 = 3;
+            int num4 = num1;
+
+            bool isTrue = true; // inte "Boolenan" eller "True"
+
+            if ((isTrue == true) && ((num1 > num3) && (num2 < num4)))
+            {
+                Console.WriteLine("(isTrue == true) && ((num1 > num3) && (num2 < num4)) is true.");
+            }
+            else
+            {
+                Console.WriteLine("(isTrue == true) && ((num1 > num3) && (num2 < num4)) is false.");
+            }
+
+            if ((isTrue == true) || ((num1 < num3) || (num2 > num4)))
+            {
+                Console.WriteLine("(isTrue = true) || ((num1 < num3) || (num2 > num4)) is true");
+            }
+            else
+            {
+                Console.WriteLine("(isTrue == true) || ((num1 < num3) || (num2 > num4)) is false");
+            }
+
+            if (!isTrue == true)
+            {
+                Console.WriteLine("Bool isTrue is not 'false'.");
+            }
+            else
+            {
+                Console.WriteLine("Bool isTrue is not 'true'");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
