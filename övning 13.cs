86e81969c954ebb985c281f0fcc2cb1bcb5b143d
nagobaa <fej@hotmail.fi>
2024-11-27 12:24:57 +0200

    Create övning 13.cs

---
+++ övning 13.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace test
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // användarninmatning
+            Console.Write("Please enter a number: ");
+            string numberInput = Console.ReadLine();
+            int number = int.Parse(numberInput);
+
+            //granskning av jämt eller udda
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"The number {number} is even.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is odd.");
+            }
+
+        Console.ReadKey();
+
+        }
+    }
+}
