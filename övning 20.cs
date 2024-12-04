ed036b13ce1c0d1aa51a3d5bc5f48dd673f8bb11
nagobaa <fej@hotmail.fi>
2024-12-04 13:51:09 +0200

    Create övning 20.cs

---
+++ övning 20.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgift20
+{
+    class Circle
+    {
+    
+        public double Radius { get; set; }
+
+        public Circle (double radius)
+        {
+            Radius = radius;
+        }
+        
+        // void är för funktioner som återlämnar inga värden
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle1 = new Circle(1.5);
+            double area = circle1.CalculateArea();
+            Console.WriteLine($"Area of the circle is: {area}");
+        }
+    }
+}
