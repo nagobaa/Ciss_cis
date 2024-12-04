5af1f3584cbae99a338cf2977cb1bb475c54cd01
nagobaa <fej@hotmail.fi>
2024-12-04 09:31:19 +0200

    Create övning 19.cs

---
+++ övning 19.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgift19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = new int[] { 1, 2, 3, 4 };
+
+            int newSum = SumOfNumbers(array); // detta behövs innan återlämningsvärdet används
+
+            if (newSum > -1)
+                Console.WriteLine($"The total is: {newSum}");
+            else
+                Console.WriteLine("Cannot add up to an empty array");
+            
+            int length = array.Length;
+            Console.WriteLine($"The length of the array is {length}.");
+        }
+
+        static int SumOfNumbers(int[] x) // inte bara (x)
+        {
+            // summan av fältet ska räknas här
+            int newSum = x.Sum();
+            return newSum;
+        }
+    }
+}
+
+
