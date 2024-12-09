ca5857110c3c495549e30c269177fc50520562d3
nagobaa <fej@hotmail.fi>
2024-12-09 12:43:00 +0200

    Update övning 9.cs

--- övning 9.cs
+++ övning 9.cs
@@ -11,36 +11,30 @@ namespace test
             int num3 = 3;
             int num4 = num1;
 
-            bool isTrue = true; // inte "Boolenan" eller "True"
+            bool isTrue = true; // inte "Boolean" eller "True"
+            bool resultAnd = isTrue && ((num1 > num3) && (num2 < num4);
+            Console.WriteLine("Logical AND: " + resultAnd);
 
-            if ((isTrue == true) && ((num1 > num3) && (num2 < num4)))
-            {
-                Console.WriteLine("(isTrue == true) && ((num1 > num3) && (num2 < num4)) is true.");
-            }
-            else
-            {
-                Console.WriteLine("(isTrue == true) && ((num1 > num3) && (num2 < num4)) is false.");
-            }
+            bool resultOr = isTrue || (num1 > num3) && (num2 < num4);
+            Console.WriteLine("Logical OR: " + resultOr);
 
-            if ((isTrue == true) || ((num1 < num3) || (num2 > num4)))
-            {
-                Console.WriteLine("(isTrue = true) || ((num1 < num3) || (num2 > num4)) is true");
-            }
-            else
-            {
-                Console.WriteLine("(isTrue == true) || ((num1 < num3) || (num2 > num4)) is false");
-            }
+            bool resultNot = !isTrue;
+            Console.WriteLine("Logical NOT: " + resultNot);
 
-            if (!isTrue == true)
-            {
-                Console.WriteLine("Bool isTrue is not 'false'.");
-            }
-            else
-            {
-                Console.WriteLine("Bool isTrue is not 'true'");
-            }
+            Console.WriteLine("num1 > num3: " + (num1 > num3));
+            Console.WriteLine("num2 < num4: " + (nnum2 < num4));
+            Console.WriteLine("num2 1 == num4: " + (num1 == num4));
+            Console.WriteLine("num2 != num3: ") + (num2 != num3));
 
-            Console.ReadKey();
+            bool expression1 = (num1 > num3 && (num2 < num4));
+            bool expression2 = (num1 < num3 || num2 > num4));
+
+            Console.WriteLine("Expression 1: " + expression1);
+            Console.WriteLine("Expression 2: " + expression2);
+
+            Console.WriteLine("Program executed succesfully");
+
+            Console.ReadLine();
         }
     }
 }
