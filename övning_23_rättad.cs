0062cc7cd6da85db377e8df9ecf4a3c1885f91d8
nagobaa <fej@hotmail.fi>
2024-12-17 17:27:02 +0200

    Create övning_23_rättad.cs

---
+++ övning_23_rättad.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgift_23_korrekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////version 1
+            //Console.WriteLine("Version 1");
+            ////användarinmatning
+            //Console.Write("Please give a value to a string variabel: ");
+
+            //try
+            //{
+            //    int num = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine($"Conversion succeeded: {num}");
+            //}
+            //// bokstäver
+            //catch (FormatException)
+            //{
+            //    Console.WriteLine("Please do not enter letters.");
+            //}
+            //// för stora eller små värden
+            //catch (OverflowException)
+            //{
+            //    Console.WriteLine("Please input a value smaller than 2 billions");
+            //}
+            //// övriga fel
+            //catch (Exception e)
+            //{
+            //    // detta visar ett allmänt felmeddelande
+            //    Console.WriteLine($"Error: {e.Message}");
+            //}
+
+            //version 2
+            Console.WriteLine("Version 2");
+
+            // användarinmatning
+            Console.Write("Please give a value to a string variabel: ");
+            // variabeln definieras
+            string variabel = Console.ReadLine();
+
+            /*Det är bättre att ha alltid relaterat till en viss uppgift
+            inom en funktion istället för att ha det skrivet in i 
+            huvudfunktionen */
+
+            //funktionen påkallas
+            if (function(variabel, out int x))
+            {
+                Console.WriteLine($"The conversion was successful: {x}");
+            }
+            else
+            {
+                Console.WriteLine($"The conversion was unsuccessful"); // inte 
+            }
+
+            // funktion
+            static bool function(string x, out int y) // inte "out y"
+            {
+                try
+                {
+                    // omvandling av textremsan till ett heltal
+                    y = Convert.ToInt32(x); // inte "Console.ReadLine(x)" bara "x"
+                    return true;
+                }
+                // bokstäver
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please do not enter letters. Please enter an integer.");
+                }
+
+                // för stora eller små bokstäver
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Please input an integer value smaller than 2 billions");
+                }
+                // övriga fel
+                catch (Exception e)
+                {
+                    // detta visar ett allmänt felmeddelande
+                    Console.WriteLine($"Error: {e.Message}");
+                }
+                y = 0;
+                return false;
+            }
+        }
+    }
+}
